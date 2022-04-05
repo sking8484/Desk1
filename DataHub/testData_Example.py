41b97dc03b068d59946b9350cbef38e2a694f338
Darst King <darstking@Darsts-MacBook-Pro.local>
2022-04-04 20:52:44 -0700

    Modified example script

--- DataHub/testData_Example.py
+++ DataHub/testData_Example.py
@@ -11,54 +11,6 @@ from dataLink import dataLink
 import numpy as np
 
 """
-credents = credentials()
-## How to get stock data
-sandboxBaseUrl = "https://sandbox.iexapis.com/"
-version = "stable/"
-token = "?token=%s&chartCloseOnly=true" % credents.iexToken
-myParams = "stock/TQQQ/chart/6m"
-base_url = sandboxBaseUrl + version + myParams + token
-
-data = requests.get(base_url).json()
-df = pd.DataFrame.from_dict(data)[['date','close']]
-df.iloc[0,0] = 'PENIS'
-
-
-
-## How to connect to google cloud mysql database
-
-
-
-"""
-#To Create a database, remove the database name in configs. Then connect without that table, and then add the new table into the connection configs.
-"""
-
-##creating new database
-#cursor = cnx.cursor()
-#query1 = "CREATE DATABASE testdb"
-#cursor.execute(query1)
-#cnx.close()
-
-
-## create a connection
-cnxn = sqlconnection.connect(**credents.testCredentials)
-cursor = cnxn.cursor()
-
-#create a table
-query2 = "CREATE TABLE testData9 (\
-                        dateVal TEXT, \
-                        close FLOAT(10))"
-
-#cursor.execute(query2)
-#cnxn.commit()
-
-## Add data to a table 
-query3 = ("INSERT INTO testData9 (dateVal, close) VALUES (%s, %s)")
-for i in range(len(df.index)):
-    print(tuple(df.iloc[i,].values))
-    cursor.execute(query3, tuple(df.iloc[i,].values))
-cnxn.commit()
-
 cursor.execute("SELECT * FROM testData9")
 out = cursor.fetchall()
 db = pd.DataFrame(out)
