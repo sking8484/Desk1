f4cdb995e33edbf9aacd2ed256f4d8ffbc79636c
Darst King <darstking@Darsts-MacBook-Pro.local>
2022-04-19 18:59:56 -0700

    Added link to DataHub Path

--- ella.py
+++ ella.py
@@ -1,5 +1,11 @@
+import os 
+import sys 
+fpath = os.path.join(os.path.dirname(__file__),'DataHub')
+sys.path.append(fpath)
+
 from DataHub.dataHub import dataHub
 from ion import ion
+
 
 class ella:
     def __init__(self):
