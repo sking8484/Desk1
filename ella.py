67336a86f49a493752a5bf52e5b087ceeaf77b4d
sking8484 <sking8484@gmail.com>
2022-08-14 22:01:08 -0400

    Adding decorators (call back funcs) removing unnecessary while timing loops

--- ella.py
+++ ella.py
@@ -29,90 +29,78 @@ class ella:
         datahub = dataHub()
         datahub.maintainData()
 
+    def checkTiming(func):
+        def wrapper(self, timingId):
+            lastUpdate = ""
+            while True:
+                if self.TimeRules.getTiming(lastUpdate, timingId):
+                    func(self)
+                    lastUpdate = date.today().strftime("%Y-%m-%d")
+                else:
+                    time.sleep(self.credents.sleepSeconds)
+        return wrapper
 
+    @checkTiming
     def optimize(self) -> None:
+        try:
+            data = pd.read_csv(self.credents.stockPriceFile).pivot(index = 'date', columns = 'symbol', values = 'value').reset_index()
+            predictions = pd.read_csv(self.credents.networkPredictionsLocation)
+            weights_df = self.ion.getOptimalWeights(data, delta = 135, leverageAmt=1.9, predictions=predictions, usePredictions=True)
+            weights_df.to_csv(self.credents.weightsFile, index = False)
 
-        lastUpdate = ""
-        while True:
-            if self.TimeRules.getTiming(lastUpdate, ['optimize']):
-                try:
-                    lastUpdate = date.today().strftime("%Y-%m-%d")
-                    data = pd.read_csv(self.credents.stockPriceFile).pivot(index = 'date', columns = 'symbol', values = 'value').reset_index()
-                    predictions = pd.read_csv(self.credents.networkPredictionsLocation)
-                    weights_df = self.ion.getOptimalWeights(data, delta = 135, leverageAmt=1.9, predictions=predictions, usePredictions=True)
-                    weights_df.to_csv(self.credents.weightsFile, index = False)
+        except Exception as e:
+            print(traceback.print_exc())
 
-                except Exception as e:
-                    print(traceback.print_exc())
-            else:
-                time.sleep(self.credents.sleepSeconds)
-
+    @checkTiming
     def updateWeights(self) -> None:
-        lastUpdate = ""
+
 
         """
         This checks if we are between 6 and 7 am PST, and then gets the data, calculates the weights and uploads to SQL
         Most of this will be abstracted away.
         """
 
-        while True:
-            if self.TimeRules.getTiming(lastUpdate, ['updateWeights']):
-                try:
-                    lastUpdate = date.today().strftime("%Y-%m-%d")
-                    DataLink = dataLink(self.credents.credentials)
-                    weights_df = pd.read_csv(self.credents.weightsFile)
-                    predictions_df = pd.read_csv(self.credents.networkPredictionsLocation)
-                    DataLink.append(self.credents.weightsTable,weights_df)
-                    DataLink.append(self.credents.networkPredictionsTable, predictions_df)
+        try:
+            DataLink = dataLink(self.credents.credentials)
+            weights_df = pd.read_csv(self.credents.weightsFile)
+            predictions_df = pd.read_csv(self.credents.networkPredictionsLocation)
+            DataLink.append(self.credents.weightsTable,weights_df)
+            DataLink.append(self.credents.networkPredictionsTable, predictions_df)
 
-                except Exception as e:
-                    print("The following error occured at " + datetime.now().strftime("%Y-%m-%d-%H-%M"))
-                    print(traceback.print_exc())
+        except Exception as e:
+            print("The following error occured at " + datetime.now().strftime("%Y-%m-%d-%H-%M"))
+            print(traceback.print_exc())
 
-            else:
-                time.sleep(self.credents.sleepSeconds)
-
+    @checkTiming
     def rebalance(self) -> None:
-        lastUpdate = ""
 
         """
         This checks if we are between 6 and 7 am PST, and then gets the data, calculates the weights and uploads to SQL
         Most of this will be abstracted away.
         """
+        try:
+            threadRebalance()
+        except Exception as e:
+            print(traceback.print_exc())
 
-        while True:
-            if self.TimeRules.getTiming(lastUpdate, ['rebalance']):
-                try:
-                    lastUpdate = date.today().strftime("%Y-%m-%d")
-                    threadRebalance()
-                except Exception as e:
-                    print(traceback.print_exc())
-            else:
-                time.sleep(self.credents.sleepSeconds)
 
     def runReportingSuite(self):
         self.ReportingSuite = reportingSuite()
         self.ReportingSuite.maintainData()
 
-
-
+    @checkTiming
     def neuralNetworkPreperation(self):
-        lastUpdate = ''
 
-        while True:
-            if self.TimeRules.getTiming(lastUpdate, ['deepLearning', 'neuralNetworkPreperation']):
-                try:
-                    lastUpdate = date.today().strftime("%Y-%m-%d")
-                    DataLink = dataLink(self.credents.credentials)
-                    prices = DataLink.returnTable(self.credents.mainStockTable)
-                    factors = DataLink.returnTable(self.credents.mainFactorTable)
+        try:
+            DataLink = dataLink(self.credents.credentials)
+            prices = DataLink.returnTable(self.credents.mainStockTable)
+            factors = DataLink.returnTable(self.credents.mainFactorTable)
 
-                    prices.to_csv(self.credents.networkPricesLocation, index = False)
-                    factors.to_csv(self.credents.networkFactorsLocation, index = False)
-                except Exception as e:
-                    print(traceback.print_exc())
-            else:
-                time.sleep(self.credents.sleepSeconds)
+            prices.to_csv(self.credents.networkPricesLocation, index = False)
+            factors.to_csv(self.credents.networkFactorsLocation, index = False)
+        except Exception as e:
+            print(traceback.print_exc())
+
 
     def neuralNetwork(self):
         orionObj = orion()
@@ -134,7 +122,7 @@ for key in options:
 
 programSelector = input("Please select which program to run \n" + selectionString)
 if programSelector == "-opt":
-    t3 = threading.Thread(target = controller.optimize).start()
+    t3 = threading.Thread(target = controller.optimize, args = (['optimize'],)).start()
 elif programSelector == '-net':
     t7 = threading.Thread(target = controller.neuralNetwork).start()
 elif programSelector == '-hub':
@@ -146,10 +134,10 @@ elif programSelector == '-hub':
 
     controller.reportingSuite = reportingSuite()
     t1 = threading.Thread(target = controller.runDataHub).start()
-    t2 = threading.Thread(target = controller.rebalance).start()
+    t2 = threading.Thread(target = controller.rebalance, args = (['rebalance'],)).start()
     t4 = threading.Thread(target = controller.runReportingSuite).start()
-    t5 = threading.Thread(target = controller.updateWeights).start()
-    t6 = threading.Thread(target = controller.neuralNetworkPreperation).start()
+    t5 = threading.Thread(target = controller.updateWeights, args = (['updateWeights'],)).start()
+    t6 = threading.Thread(target = controller.neuralNetworkPreperation, args = (['deepLearning', 'neuralNetworkPreperation'],)).start()
 
 
 
