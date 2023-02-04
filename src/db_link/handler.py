def94119841a4d7eb1ebf88a05c9a7ce7c2a12e9
Darst King <41591910+sking8484@users.noreply.github.com>
2023-02-04 14:09:44 -0500

    Lambda requirements (#13)
    * adjusting makefile
    
    * Adding handler to each file --- make sure to adjust handler

--- src/db_link/handler.py
+++ src/db_link/handler.py
@@ -1,4 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from db_link.db_link import test_setup
 
-def handler():
+def handler(response, context):
     test_setup()
