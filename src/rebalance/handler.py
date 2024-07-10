e77c105748427507f9b1e0d001930eacfc197fe7
sking8484 <sking8484@gmail.com>
2024-07-10 10:30:52 -0400

    reducing risk weight and having reporting suite linked to rebalace

--- src/rebalance/handler.py
+++ src/rebalance/handler.py
@@ -24,4 +24,12 @@ def handler(req, resp):
     rebalance = Rebalance(alpacaLink)
     rebalance.placeTrades(orders)
 
+    sendMessage()
+
     return {'statusCode': 200}
+
+def sendMessage():
+    if os.environ["ENV"] != "local":
+        sqs_client = boto3.client("sqs")
+        queueUrl = sqs_client.get_queue_url(QueueName="reportingsuite-queue")["QueueUrl"]
+        sqs_client.send_message(QueueUrl=queueUrl, MessageBody="Hi")
